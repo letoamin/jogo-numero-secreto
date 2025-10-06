42499212d8c62632ead26594972616a7d9e67fee
Omar Amin <tp.omar.amin@totvspartners.com.br>
2025-10-06 19:19:04 -0300

    Segundo commit do projeto

---
+++ app.js
@@ -0,0 +1,8 @@
+alert('Bem vindo ao jogo do Número secreto!');
+let numeroSecreto = 29;
+let chute = prompt('Escolha um número entre 1 e 30');
+
+if (chute == numeroSecreto) {
+
+    console.log('Isso aí! Você descobriu o número secreto!')
+};
