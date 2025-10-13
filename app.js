66a504bcfd5f39fcfb82870f00887a77d7a8d84a
Omar Amin <tp.omar.amin@totvspartners.com.br>
2025-10-13 14:38:29 -0300

    Correção do numero secreto

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 alert('Bem vindo ao jogo do Número secreto!');
-let numeroSecreto = 29;
+let numeroSecreto = 5;
+console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 30');
-
+// comentário
 if (chute == numeroSecreto) {
-
-    console.log('Isso aí! Você descobriu o número secreto!')
-}; 
+   alert('Acertou!');
+} else {
+  alert('Errou!');
+};
